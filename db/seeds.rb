c98474538491e0843508d81afad8cd227806ac9b
Janice <janicelaset@gmail.com>
2015-10-17 12:32:03 -0700

    seed questions and clus

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,24 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-  Clue.create([{ description: 'Clue 1', question: 'Question 1', answer: 'answer1', display: false },
-               { description: 'Clue 2', question: 'Question 2', answer: 'answer2', display: false },
-               { description: 'Clue 3', question: 'Question 3', answer: 'answer3', display: false },
-               { description: 'Clue 4', question: 'Question 4', answer: 'answer4', display: false },
-               { description: 'Clue 5', question: 'Question 5', answer: 'answer5', display: false }])
+  Clue.create([
+    { description: 'On these grounds is where I spend most of my days.',
+      question: "Pop quiz, Aliens invade earth, there's one cheesecake slice left, there's a zombie hundred feet away, what do you do?",
+      answer: 'shoot the hostage',
+      display: false },
+    { description: "After the sun crosses the meridian, I take this path to health.",
+      question: "What's my favorite?",
+      answer: 'smiling',
+      display: false },
+    { description: "I just wanted to tell you I love you and I think we're taylor made for each other.",
+      question: "Who is the phenomenon where one happens upon some obscure piece of information and soon afterwards encounters the same subject again?",
+      answer: 'david hasselhoff',
+      display: false },
+    { description: "You won't be the first to explore this area, but someone was.",
+      question: 'An invisible space that could only be visited on the phone.',
+      answer: 'third space',
+      display: false },
+    { description: "Some infinities are bigger than other infinities. Look at it from a different perspective, you'll see three infinities are also bigger than one.",
+      question: "Do you know my most preferred color?",
+      answer: 'yes',
+      display: false }])
